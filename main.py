6fff3feced678705a4318fbd3354cd4da8403480
showgamesaz9 <30788134-showgamesaz9@users.noreply.replit.com>
2024-01-19 16:47:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,162 @@
+
+# name = input("Твоё имя: ")
+# city = input("Твой город: ")
+# print("Твоё имя: ", name)
+# print("Твой город: ", city)
+
+# print(f"Твоё имя: {name}. Твой город: {city}")
+
+# number1 = int(input("Введи 1 число: "))
+# number2 = int(input("Введи 2 число: "))
+# print(number1 + number2)
+# answer1 = input("Какая твоя любимая игра?")
+# answer2 = int(input("Насколько ты хорош(а) в этой игре от 1 до 10?"))
+
+# print(answer1)
+# print(answer2)
+
+# hour = int(input("Какой сейчас час?"))
+
+# day = int(input("Какой сейчас день?"))
+
+# mounth = int(input("Какой сейчас месяц?"))
+
+# year = int(input("Какой сейчас год?"))
+
+# print("Сейчас", hour, "часов", day, "число", mounth, "месяц", year, "год")
+
+# print("Добро пожаловать в квиз по стартапам!")
+
+# print("Ответь на следующие вопросы")
+
+# question = "Как называется компания, которая создается для развития новой идеи или инновационного продукта?: "
+
+# answer = "Стартап"
+
+# print(question)
+
+# user_input = input("Введи свой ответ: ")
+
+# print(user_input)
+
+# if user_input == answer.lower():
+#     print("Правильно!")
+# else:
+#     print("Неправильно!")
+
+# a = int(5.99999)
+# print(a)
+
+# var = int(input())
+# print(var)
+
+# message = 'hello\nivan'
+# print 
+
+# x = 5
+# print(type(x))
+
+# x = int(float(input()))
+# print(x)
+
+# message = 'hello'
+# name = 'zakhar'
+# print(message + ' ' + name)
+
+# print(type(3))
+
+# numbers = 5
+# letters = "Я программист!"
+# print(numbers,letters)
+
+# a = int('5')
+# b = int('4')
+# print(a + b)
+
+# projects = ["Сайт для отеля", "Игра-стрелялка", "Калькулятор", "Видеохостинг"]
+# print(projects[2])
+
+# print("Добро пожаловать в квиз по стартапам!")
+# print("Ответь на следующие вопросы:")
+
+# questions = [
+#     "1. Как называется компания, которая создается для развития новой идеи или инновационного продукта?",
+#     "2. Как назвается человек или компания, который вкладывает деньги в бизнес с целью получения прибыли?",
+#     "3. Как называется капитал, который дают инвесторы на развитие стартапа?",
+#     "4. Как пишется минимально жизнеспособный продукт, который создается для тестирования идей и концепций?",
+#     "5. Какой план создают перед тем, как открывать стартап и занимать деньги?",
+#     "6. Как называются другие компании на рынке, которые предлагают аналогичные товары или услуги?",
+#     "7. Как называется разница между выручкой и затратами компании?"
+# ]
+
+# answers = [
+#     "Стартап", "Инвестор", "Инвестиция", "MVP", "Бизнес-план", "Конкуренты", "Прибыль"
+# ]
+
+# score = 0
+
+
+# print(questions[0])
+# user_answer = input("Введи свой ответ: ")
+# if user_answer.lower() == answers[0].lower():
+#     print("Правильно!")
+#     score = score + 1
+# else:
+#     print("Неправильно.")
+
+# print(questions[1])
+# user_answer = input("Введи свой ответ: ")
+# if user_answer.lower() == answers[1].lower():
+#     print("Правильно!")
+#     score = score + 1
+# else:
+#     print("Неправильно.")
+
+# print(questions[2])
+# user_answer = input("Введи свой ответ: ")
+# if user_answer.lower() == answers[2].lower():
+#     print("Правильно!")
+#     score = score + 1
+# else:
+#     print("Неправильно.")
+
+# print(questions[3])
+# user_answer = input("Введи свой ответ: ")
+# if user_answer.lower() == answers[3].lower():
+#     print("Правильно!")
+#     score = score + 1
+# else:
+#     print("Неправильно.")
+
+# print(questions[4])
+# user_answer = input("Введи свой ответ: ")
+# if user_answer.lower() == answers[4].lower():
+#     print("Правильно!")
+#     score = score + 1
+# else:
+#     print("Неправильно.")
+
+# print(questions[5])
+# user_answer = input("Введи свой ответ: ")
+# if user_answer.lower() == answers[5].lower():
+#     print("Правильно!")
+#     score = score + 1
+# else:
+#     print("Неправильно.")
+
+# print(questions[6])
+# user_answer = input("Введи свой ответ: ")
+# if user_answer.lower() == answers[6].lower():
+#     print("Правильно!")
+#     score = score + 1
+# else:
+#     print("Неправильно.")
+
+# if score > 5:
+#     print("Это отличный результат! Ты много знаешь о стартапах.")
+# elif score > 3:
+#     print("Неплохой результат! Как здорово, что тебе предстоит узнать ещё так много о стартапах.")
+# else:
+#     print("Видимо, ты только начинаешь свой путь к стартапам! Ты ещё много чего узнаешь о мире, где мы живём.")
+
+print('hello world')
